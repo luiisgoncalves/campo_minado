8d13c1962dfd6ee34317eae507062b703b6138ae
luis <luiis.goncalves@hotmail.com>
2022-07-29 00:02:57 -0300

    detecção de escolhas em areas livres de minas, proximas às minas e de escolhas exatamente em minas [seed(45) -> testar com campo 15x15]

--- classes.py
+++ classes.py
@@ -1,5 +1,7 @@
 import numpy as np
-from random import randint
+from random import randint, seed
+
+seed(47)
 
 
 class Campo:
@@ -33,7 +35,6 @@ class CampoMinado(Campo):
         super().__init__(linhas, colunas, elemento)
         self.dificuldade = dificulade
         self.bomba = bomba
-        self.maximo = self.linhas * self.colunas
         self.campo_minado = np.copy(self.campo)
 
     def cria_bombas(self):
@@ -62,30 +63,10 @@ class CampoMinado(Campo):
             self.campo_minado[linha][coluna] = self.bomba
 
     def conta_bombas(self):
-        dimensao = [-1, 0, 1]
-
         for linha in range(self.linhas):
             for coluna in range(self.colunas):
 
-                matriz_index = np.array([], dtype=int)
-
-                ###########################################################################################################
-                # line_inf = linha - 1 if linha - 1 >= 0 else 0
-                # line_sup = linha + 1
-                # column_inf = coluna - 1 if coluna - 1 >= 0 else 0
-                # column_sup = coluna + 1
-                # matriz_index = np.append(matriz_index, self.campo_minado[line_inf:line_sup+1, column_inf:column_sup+1])
-                # print(matriz_index)
-                ###########################################################################################################
-
-                for i in range(3):
-                    for j in range(3):
-                        line = linha + dimensao[i]
-                        column = coluna + dimensao[j]
-
-                        matriz_index = np.append(matriz_index, [line, column])
-
-                matriz_index = matriz_index.reshape((9, 2))
+                matriz_index = self.redondezas(linha, coluna)
 
                 if self.campo_minado[linha][coluna] != self.bomba:
                     qntd_bomba = 0
@@ -94,3 +75,99 @@ class CampoMinado(Campo):
                         if 0 <= column < self.linhas and 0 <= line < self.colunas:
                             qntd_bomba = qntd_bomba + 1 if self.campo_minado[line][column] == self.bomba else qntd_bomba
                     self.campo_minado[linha][coluna] = qntd_bomba
+
+    def ver_elementos(self, linha, coluna):
+        matriz = self.redondezas(linha, coluna)
+        sub_matriz = np.array([], dtype=int)
+
+        for elemento in matriz:
+            if 0 <= elemento[0] < self.linhas and 0 <= elemento[1] < self.colunas:
+                item = int(self.campo_minado[elemento[0], elemento[1]]) if self.campo_minado[elemento[0], elemento[1]] != self.bomba else -1
+                sub_matriz = np.append(sub_matriz, item)
+
+        match len(sub_matriz):
+            case 4:
+                sub_matriz = sub_matriz.reshape((2, 2))
+            case 6:
+                if matriz[0, 1] == -1 or matriz[9, 1] == self.colunas:
+                    sub_matriz = sub_matriz.reshape((3, 2))
+                else:
+                    sub_matriz = sub_matriz.reshape((2, 3))
+            case 9:
+                sub_matriz = sub_matriz.reshape((3, 3))
+
+        # print(sub_matriz)
+        return sub_matriz
+
+    def redondezas(self, linha, coluna):
+        dimensao = [-1, 0, 1]
+        matriz_index = np.array([], dtype=int)
+
+        ###########################################################################################################
+        # line_inf = linha - 1 if linha - 1 >= 0 else 0
+        # line_sup = linha + 1
+        # column_inf = coluna - 1 if coluna - 1 >= 0 else 0
+        # column_sup = coluna + 1
+        # matriz_index = np.append(matriz_index, self.campo_minado[line_inf:column_inf, line_sup:column_sup])
+        # print(matriz_index)
+        ###########################################################################################################
+
+        for i in range(3):
+            for j in range(3):
+                line = linha + dimensao[i]
+                column = coluna + dimensao[j]
+
+                if 0 <= line < self.linhas and 0 <= column < self.colunas:
+                    matriz_index = np.append(matriz_index, [line, column])
+        # print(matriz_index.reshape((9, 2)))
+        # print('\n')
+        return matriz_index.reshape((-1, 2))
+
+    def escolha(self, linha, coluna):
+        if self.campo_minado[linha, coluna] == self.bomba:
+            self.explosao()
+        elif int(self.campo_minado[linha, coluna]) == 0:
+            livres = self.sem_bomba(linha, coluna)
+            for livre in livres:
+                line, column = livre
+                self.campo[line, column] = self.campo_minado[line, column]
+        else:
+            self.campo[linha, coluna] = self.campo_minado[linha, coluna]
+
+    def explosao(self):
+        for i in range(self.linhas):
+            for j in range(self.colunas):
+                if self.campo_minado[i, j] == self.bomba:
+                    self.campo[i, j] = self.bomba
+
+    def sem_bomba(self, linha, coluna):
+        ja_usados = np.array([], dtype=int)
+        zeros = np.array([linha, coluna])
+        elementos_visiveis = self.redondezas(linha, coluna)
+
+        while zeros.any():
+            for i in range(len(elementos_visiveis)):
+                zeros = zeros.reshape((-1, 2))
+                elemento = elementos_visiveis[i]
+
+                contido_ja_usado = False
+                for n in range(len(ja_usados)):
+                    if np.isin(elemento, ja_usados[n]).all():
+                        contido_ja_usado = True
+
+                contido_zeros = False
+                for n in range(len(zeros)):
+                    if np.isin(elemento, zeros[n]).all():
+                        contido_zeros = True
+
+                item = int(self.campo_minado[elemento[0], elemento[1]])
+                if item == 0 and not contido_zeros and not contido_ja_usado:
+                    zeros = np.append(zeros, elemento).astype(int)
+
+            zeros = zeros.reshape((-1, 2))
+            zeros = np.delete(zeros, [0, 1])
+            ja_usados = np.append(ja_usados, elementos_visiveis).reshape((-1, 2))
+            if zeros.any():
+                elementos_visiveis = self.redondezas(zeros[0], zeros[1])
+        ja_usados = np.unique(ja_usados, axis=0)
+        return ja_usados
